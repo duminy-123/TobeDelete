7d970acf5de3d8aa97f5c798b2744c662f7b7ad8
duminy-123 <58914095+duminy-123@users.noreply.github.com>
2021-08-24 20:24:22 +0530

    Add a scatter plot

--- sample1.py
+++ sample1.py
@@ -1,6 +1,7 @@
 # Import libraries
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 df = pd.DataFrame({'colA': [10, 20, 30],
                    'colB': [100, 200, 300]})
@@ -8,3 +9,11 @@ df = pd.DataFrame({'colA': [10, 20, 30],
 print(df)
 
 -- second line
+
+plt.scatter(x=df['colA'],y=df['colB'])
+plt.title('Scatter Plot')
+plt.xlabel('X Label')
+plt.ylabel('Y Label')
+plt.savefig('Scatter Plot')
+
+
