d3423a6144f4c784fb36fd925e245d524b6f0fe1
duminy-123 <58914095+duminy-123@users.noreply.github.com>
2021-08-24 20:42:50 +0530

    Update sample1.py

--- sample1.py
+++ sample1.py
@@ -8,7 +8,6 @@ df = pd.DataFrame({'colA': [10, 20, 30],
 
 print(df)
 
--- second line
 
 plt.scatter(x=df['colA'],y=df['colB'])
 plt.title('Scatter Plot')
@@ -16,5 +15,4 @@ plt.xlabel('X Label')
 plt.ylabel('Y Label')
 plt.savefig('Scatter Plot')
 
--- Third line
---Another Line
+
